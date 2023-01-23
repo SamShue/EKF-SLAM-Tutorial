d6778f879fe6d0a2beb06fc5cfd1b7309179d57f
Sam Shue <evilrobotfactory@gmail.com>
2023-01-23 11:06:05 -0500

    Changed robotPose from vector to struct

--- SIMULATION_DeadReckoning.m
+++ SIMULATION_DeadReckoning.m
@@ -3,29 +3,31 @@ clear all;
 close all;
 
 % Robot parameters
-robotPose = [2,3,deg2rad(45)];
+robotPose.x = 2.0;
+robotPose.y = 3.0;
+robotPose.theta = deg2rad(45);
 
 % Populate wheel velocities vector
 vl_mps = 0.7;
 vr_radps = deg2rad(20);
 % Linear and Angular Velocity Measurements
-u = [vl_mps, vr_radps; vl_mps, vr_radps];
+u = [vl_mps, vr_radps; vl_mps, vr_radps; vl_mps, vr_radps; vl_mps, vr_radps];
 % time between iterations in seconds
 dt_s = 0.5;
 
 hold on;
-drawRobot(robotPose(1), robotPose(2), rad2deg(robotPose(3)), 0.1);
-xpath = [robotPose(1)]; ypath =[robotPose(2)];
+drawRobot(robotPose.x, robotPose.y, rad2deg(robotPose.theta), 0.1);
+xpath = [robotPose.x]; ypath =[robotPose.y];
 
-for ii = 1:length(u(1,:))
-    robotPose(1) = robotPose(1) + u(ii,1)*cos(robotPose(3))*dt_s;
-    robotPose(2) = robotPose(2) + u(ii,1)*sin(robotPose(3))*dt_s;
-    robotPose(3) = robotPose(3) + vr_radps*dt_s;
+for ii = 1:length(u(:,1))
+    robotPose.x = robotPose.x + u(ii,1)*cos(robotPose.theta)*dt_s;
+    robotPose.y = robotPose.y + u(ii,1)*sin(robotPose.theta)*dt_s;
+    robotPose.theta = robotPose.theta + vr_radps*dt_s;
     
-    drawRobot(robotPose(1), robotPose(2), rad2deg(robotPose(3)), 0.1);
-    xpath = [xpath; robotPose(1)]; ypath =[ypath; robotPose(2)];
+    drawRobot(robotPose.x, robotPose.y, rad2deg(robotPose.theta), 0.1);
+    xpath = [xpath; robotPose.x]; ypath =[ypath; robotPose.y];
 end
 
 plot(xpath, ypath);
-xlim([1.5 3.]); ylim([2.5 4.0]);
+xlim([1.75 3.0]); ylim([2.5 4.5]);
 xlabel('meters'); ylabel('meters');
